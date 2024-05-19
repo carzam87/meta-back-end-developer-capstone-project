4469c007c02328e99a50190eed428196f585febe
Carlos Zambrana <carzam87@gmail.com>
2024-05-19 11:26:28 -0600

    Setup WIP

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', main_data)
