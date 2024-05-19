0d68838a5a8850c59ffdfad7e5a3454c4da65c98
Carlos Zambrana <carzam87@gmail.com>
2024-05-19 11:50:36 -0600

    chore: Remove unused reservation app files and add restaurant app models and admin configuration

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from .models import Menu
 
 
-def menu(request):
-    menu_data = Menu.objects.all()
-    main_data = {"menu": menu_data}
-    return render(request, 'menu.html', main_data)
+def home(request):
+    return render(request, "index.html")
